e097a0fe72126a9e3c9ea8755ab59a4f687b469c
Lachlan Krautz <lachlan.krautz@gmail.com>
2016-06-22 12:05:34 +1000

    problems

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,30 @@ extern crate chrono;
 use std::fmt;
 use std::process::Command;
 use std::str;
+use std::iter;
 // use chrono::datetime::DateTime;
 // use chrono::Local;
 // use chrono::offset::TimeZone;
 use chrono::*;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 struct Branch {
     user: String,
-    branch: String,
     date: DateTime<FixedOffset>,
+    branch: String,
 }
 
 impl fmt::Display for Branch {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "[{}] {} {}",
+               self.date.format("%Y-%m-%d"),
+               self.user,
+               self.branch)
+    }
+}
+
+impl fmt::Debug for Branch {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,
                "user: {}\nbranch: {}\ndate: {}",
@@ -25,15 +37,14 @@ impl fmt::Display for Branch {
 }
 
 fn parse_repo(line: String) -> Option<Branch> {
-    if !line.contains("refs/remotes/origin/HEAD") && !line.contains("refs/remotes/origin/master") {
-
+    if !line.contains("origin/HEAD") && !line.contains("origin/master") {
         let mut user = String::new();
         let mut date = String::new();
         let mut branch = String::new();
         for (i, part) in line.split("^").enumerate() {
             match i {
                 0 => user = part.to_string(),
-                1 => branch = part.to_string(),
+                1 => branch = part.to_string().replace("origin/", ""),
                 2 => date = part.to_string(),
                 _ => {}
             }
@@ -49,19 +60,34 @@ fn parse_repo(line: String) -> Option<Branch> {
 }
 
 fn main() {
+    let mut v = get_git_data();
+    v.sort();
+    print_git_data(v);
+}
 
-    let v = get_git_data();
-
+fn print_git_data(v: Vec<Branch>) {
+    let max = v.iter().fold(0, |max, branch| {
+        let len = branch.user.len();
+        if len > max {
+            len
+        } else {
+            max
+        }
+    });
+    println!("max: {}", max);
     for branch in v {
-        println!("[branch]\n{}", branch);
+        let padding_count = max - branch.branch.len();
+        let padding: String = iter::repeat(" ").take(padding_count).collect();
+        print!("[{}]", branch.date.format("%Y-%m-%d"));
+        print!("{}", branch.branch);
+        println!("{}", padding);
     }
 }
 
 fn get_git_data() -> Vec<Branch> {
     let output = Command::new("git")
         .arg("for-each-ref")
-        .arg("--sort=-committerdate")
-        .arg("--format=%(authorname)^%(refname)^%(committerdate:rfc2822)")
+        .arg("--format=%(authorname)^%(refname:short)^%(committerdate:rfc2822)")
         .arg("refs/remotes/origin/")
         .output()
         .expect("failed to execute process");
