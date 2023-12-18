a8627ed141e70c2db59ecbf4b2ca596e1f6bc085
Lachlan Krautz <lachlan.krautz@gmail.com>
2023-12-18 12:06:42 +1000

    fix filter for origin/HEAD

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 extern crate chrono;
-extern crate colored;
 extern crate clap;
+extern crate colored;
 
+use chrono::{DateTime, FixedOffset, Local};
+use clap::{App, AppSettings, Arg};
+use colored::Colorize;
 use std::fmt;
+use std::iter;
 use std::process::Command;
 use std::str;
-use std::iter;
-use colored::Colorize;
-use chrono::{DateTime, Local, FixedOffset};
-use clap::{App, AppSettings, Arg};
 
 fn main() {
     let args = App::new("git-who")
         .version("0.1.3")
         .about("List remote branches by author and date of last commit")
         .setting(AppSettings::ColoredHelp)
-        .arg(Arg::with_name("remote")
-            .help("Remote name [default: origin]")
-            .short("r")
-            .long("remote")
-            .takes_value(true)
-            .value_name("NAME"))
+        .arg(
+            Arg::with_name("remote")
+                .help("Remote name [default: origin]")
+                .short("r")
+                .long("remote")
+                .takes_value(true)
+                .value_name("NAME"),
+        )
         .get_matches();
     let remote = args.value_of("remote").unwrap_or("origin");
 
@@ -38,60 +40,66 @@ struct Branch {
 
 impl fmt::Display for Branch {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "[{}] {} {}",
-               self.date.format("%Y-%m-%d"),
-               self.user,
-               self.branch)
+        write!(
+            f,
+            "[{}] {} {}",
+            self.date.format("%Y-%m-%d"),
+            self.user,
+            self.branch
+        )
     }
 }
 
 impl fmt::Debug for Branch {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "user: {}\nbranch: {}\ndate: {}",
-               self.user,
-               self.branch,
-               self.date)
+        write!(
+            f,
+            "user: {}\nbranch: {}\ndate: {}",
+            self.user, self.branch, self.date
+        )
     }
 }
 
 fn parse_repo(line: String) -> Option<Branch> {
-    if !line.contains("origin/HEAD") {
-        let mut user = String::new();
-        let mut date = String::new();
-        let mut branch = String::new();
-        for (i, part) in line.split("^~^").enumerate() {
-            match i {
-                0 => user = part.to_string(),
-                1 => branch = part.to_string().replace("origin/", ""),
-                2 => date = part.to_string(),
-                _ => {}
-            }
-        }
-        Some(Branch {
-            user,
-            branch,
-            date: DateTime::parse_from_rfc2822(&date).unwrap(),
-        })
-    } else {
-        None
+    if line.contains("^origin^") {
+        return None;
     }
+
+    let mut user = String::new();
+    let mut date = String::new();
+    let mut branch = String::new();
+    for (i, part) in line.split("^~^").enumerate() {
+        match i {
+            0 => user = part.to_string(),
+            1 => branch = part.to_string().replace("origin/", ""),
+            2 => date = part.to_string(),
+            _ => {}
+        }
+    }
+    Some(Branch {
+        user,
+        branch,
+        date: DateTime::parse_from_rfc2822(&date).unwrap(),
+    })
 }
 
 fn print_git_data(v: Vec<Branch>) {
-    let max = v.iter().fold(0, |max, branch| std::cmp::max(max, branch.user.len()));
+    let max = v
+        .iter()
+        .fold(0, |max, branch| std::cmp::max(max, branch.user.len()));
     let now = Local::now();
 
     v.iter().for_each(|branch| {
         let padding_count = max - branch.user.len();
         let padding = iter::repeat(" ").take(padding_count).collect::<String>();
 
-        println!("[{}] {}{} {}",
-                 coloured_date(now, branch.date),
-                 branch.user,
-                 padding,
-                 branch.branch);
+        println!(
+            "[{}] {}{} {}",
+            coloured_date(now, branch.date),
+            branch.user,
+            padding,
+            branch.branch
+        );
     });
 }
 
